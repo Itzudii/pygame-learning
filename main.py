d89735e756e102259850cde5d86cf85f3e2c4c9e
Uditya Patel <pateluditya2004@gmail.com>
2026-07-09 20:32:55 +0530

    inital commit

--- main.py
+++ main.py
@@ -14,10 +14,5 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    screen.fill((255,0,0))
-    
-    pygame.display.update()
-    screen.fill((255,255,0))
     
 pygame.quit()
