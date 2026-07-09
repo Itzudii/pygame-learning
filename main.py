95fa49023a3b3660c567592a83a45170f586934a
Uditya Patel <pateluditya2004@gmail.com>
2026-07-09 20:17:39 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# game loop
+running = True
+while running:
+ 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
